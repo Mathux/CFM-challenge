3fe8b7cfe0c2673b33a10ace256b8a31a39db1c7
Mathis Petrovich <mathis.petrovich@gmail.com>
2019-02-05 16:14:44 +0100

    split dataset is working

--- utils.py
+++ utils.py
@@ -8,9 +8,10 @@ Created on Thu Jan 24 21:15:21 2019
 
 import pandas as pd
 from config import *
+import numpy as np
 
 # Load train and test data
-def load_data() : 
+def load_data(): 
     X_train = pd.read_csv(path_to_train)
     y_train = pd.read_csv(path_to_train_returns,sep = ',')
     X_test = pd.read_csv(path_to_test)
@@ -19,6 +20,24 @@ def load_data() :
     y_train_labels['end_of_day_return'] = 1*(y_train['end_of_day_return']>=0)
         
     return X_train, X_test, y_train, y_train_labels
+
+
+# Split the train dataset into training and validation (keep different date)
+def split_dataset(data, split_val=0.1, seed=SEED):
+    np.random.seed(seed)
+
+    dates = data["date"].unique().copy()
+    n_dates = len(dates)
+    all_index = np.arange(n_dates)
+    np.random.shuffle(all_index)
+
+    train_index = all_index[int(split_val*n_dates):]
+    val_index = all_index[0:int(split_val*n_dates)]
+
+    train = data[data["date"].isin(dates[train_index])]
+    val = data[data["date"].isin(dates[val_index])]
+    
+    return train, val
 
 
 # Tools to give the csv format
