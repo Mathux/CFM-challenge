e656a0913e6c9808466dee979b88edc62ab48fa5
Evrard Garcelon <evrard.garcelon@gmail.com>
2019-02-16 17:19:33 +0100

    Builded the first dl model

--- model.py
+++ model.py
@@ -1,13 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 16 16:33:23 2019
+
+@author: evrardgarcelon
+"""
+
 import numpy as np
 
+import keras
 from keras.models import Model, Input
-from keras.layers import Dense, Dropout, Embedding, LSTM, Conv1D, PReLU, 
-SpatialDropout1D, concatenate
+from keras.layers import (Dense, Dropout, Embedding, Conv1D, PReLU, 
+SpatialDropout1D, concatenate, BatchNormalization, Flatten, LSTM)
 from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 from keras.optimizers import Nadam
 from keras.utils import to_categorical
 
-from process_data import Data
+from processing_data import Data
 
 from sklearn.preprocessing import LabelEncoder
 
@@ -23,7 +32,7 @@ class LSTMModel(object) :
         self.n_eqt = X['eqt_code'].unique()
         self.return_cols = [c for c in X.columns if c.endswith(':00')]
         self.non_return_cols = [c for c in X.columns if not c.endswith(':00')]
-        self.returns_length = len(return_cols)
+        self.returns_length = len(self.return_cols)
         self.lstm_out_dim = lstm_out_dim
         self.use_lstm = use_lstm
         self.dropout_rate = dropout_rate
@@ -37,46 +46,48 @@ class LSTMModel(object) :
         eqt_code_input = Input(shape =(1,), dtype = 'int64', 
                                name = 'eqt_code_input')
         eqt_emb = Embedding(output_dim = self.eqt_embeddings_size, 
-                                   input_dim = 1, input_length = self.n_eqt, 
-                                   name = 'eqt embeddings')(eqt_code_input)
+                            input_dim = 1,
+                            name = 'eqt_embeddings')(eqt_code_input)
         eqt_emb = SpatialDropout1D(self.dropout_rate)(eqt_emb)
-
+        eqt_emb = Flatten()(eqt_emb)
         
         # Then the LSTM/CNN1D for the returns time series 
-        returns_input = Input(shape = (self.return_length,1),
+        returns_input = Input(shape = (self.returns_length,1),
                               name = 'returns_input')
         
         if self.use_lstm :
-            pass
+            returns_lstm = LSTM(self.lstm_out_dim)(returns_input)
         else :
             returns_lstm = Conv1D(filters = self.lstm_out_dim, 
                                   kernel_size = self.kernel_size, 
                                   activation = 'linear',
                                   name = 'returns_conv')(returns_input)
-        returns_lstm = PReLu()(returns_lstm)
+            returns_lstm = Flatten()(returns_lstm)
+        returns_lstm = PReLU()(returns_lstm)
         returns_lstm = Dropout(self.dropout_rate)(returns_lstm)
         
         # and the the LSTM/CNN part for the volatility time series
         
-        vol_input = Input(shape = (self.return_length,1),name = 'vol_input')
+        vol_input = Input(shape = (self.returns_length,1),name = 'vol_input')
         
         if self.use_lstm :
-            pass
+            vol_lstm = LSTM(self.lstm_out_dim)(vol_input)
         else : 
             vol_lstm = Conv1D(filters = self.lstm_out_dim, 
                               kernel_size = self.kernel_size, 
                               activation = 'linear',
                               name = 'returns_conv')(vol_input)
-        vol_lstm = PReLu()(vol_lstm)
+            vol_lstm = Flatten()(vol_lstm)
+        vol_lstm = PReLU()(vol_lstm)
         vol_lstm = Dropout(self.dropout_rate)(vol_lstm)
         
         x = concatenate([eqt_emb,returns_lstm,vol_lstm])
         x = Dense(32,activation = 'linear')(x)
-        x = PReLu()(x)
+        x = PReLU()(x)
         x = BatchNormalization()(x)
         x = Dropout(self.dropout_rate)(x)
         x = Dense(32,activation = 'linear')(x)
-        x = PReLu()(x)
+        x = PReLU()(x)
         x = BatchNormalization()(x)
         x = Dropout(self.dropout_rate)(x)
         
@@ -88,11 +99,11 @@ class LSTMModel(object) :
         
         x = concatenate([handmade_features_input,x])
         x = Dense(32,activation = 'linear')(x)
-        x = PReLu()(x)
+        x = PReLU()(x)
         x = BatchNormalization()(x)
         x = Dropout(self.dropout_rate)(x)
         x = Dense(32,activation = 'linear')(x)
-        x = PReLu()(x)
+        x = PReLU()(x)
         x = BatchNormalization()(x)
         x = Dropout(self.dropout_rate)(x)
         output = Dense(1,activation = 'sigmoid')(x)
@@ -113,7 +124,7 @@ if __name__ == '__main__' :
     
     data = Data(small = True)
     X,y = data.train.data, data.train.labels
-    model= LSTMModel(X,y)
+    model= LSTMModel(X,y, use_lstm = True)
         
         
         
